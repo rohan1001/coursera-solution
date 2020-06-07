89a52fd5a1005f94813746651be3aa58b458916f
rohan1001 <rohandeshpande373@gmail.com>
2020-06-07 18:03:29 +0530

    javascript

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["rohan", "vivek", "saurabh", "pramod", "akash", "rushi", "rohit", "mamoon", "hussain", "aaquib"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
